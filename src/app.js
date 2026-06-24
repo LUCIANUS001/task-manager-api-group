08c3566f20a77e0f75114401a7e6beb8b6b45137
1-dara <1ireneokon@gmail.com>
2026-06-24 23:25:27 +0200

    Set up app.js configuration

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+
+app.get('/', (req, res) => {
+    res.send('Task Manager API is running!');
+});
+
+//app.js setup - Irene
+
+module.exports = app;
