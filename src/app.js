0f32e609ae8a21138f8b4016ee1a2763b1c5ba4a
1-dara <1ireneokon@gmail.com>
2026-06-25 09:52:39 +0200

    Add dotenv, nodemon and server setup

--- src/app.js
+++ src/app.js
@@ -1,14 +1,12 @@
 require('dotenv').config();
+
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 5000;
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Task Manager API is running!');
 });
-
-//app.js setup - Irene
 
 module.exports = app;
